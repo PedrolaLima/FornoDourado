466ceed3ef09b12c22323e240b609d4a03018d15
Alberto Theodoro <alberto.s.s.theodoro@gmail.com>
2024-11-24 19:56:53 -0300

    Added secure password hashing

---
+++ src/main/java/br/com/fatec/controller/LoginController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.fatec.controller;
+
+import br.com.fatec.dao.FuncionarioDAO;
+import br.com.fatec.data.Database;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author alberto
+ */
+public class LoginController {
+    public boolean login(){
+        FuncionarioDAO fd = new FuncionarioDAO();
+    
+        return false;
+    }
+}
