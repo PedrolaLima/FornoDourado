b0412a6347acc07aeae06838bf363af7e8148aa9
Alberto Theodoro <alberto.s.s.theodoro@gmail.com>
2024-12-01 05:10:02 -0300

    Added the remaining tables and worked on addfuncionario

--- src/main/java/br/com/fatec/controller/adicionarFuncionarioController.java
+++ src/main/java/br/com/fatec/controller/adicionarFuncionarioController.java
@@ -4,17 +4,23 @@ import br.com.fatec.App;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.function.UnaryOperator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import br.com.fatec.data.Database;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.SnapshotParameters;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextFormatter;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
@@ -38,6 +44,22 @@ public class adicionarFuncionarioController implements Initializable {
     private TextField cepField;
     @FXML
     private TextField cpfField;
+    @FXML
+    public TextField emailField;
+    @FXML
+    public TextField nameField;
+    @FXML
+    public TextField passwordField;
+    @FXML
+    public TextField addressField;
+    @FXML
+    public ComboBox<String> occupationCombo;
+    @FXML
+    public ComboBox<String> stateCombo;
+    @FXML
+    public ComboBox<String> cityCombo;
+    @FXML
+    public ComboBox<String> statusCombo;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -113,60 +135,193 @@ public class adicionarFuncionarioController implements Initializable {
         });
 
        // Máscara para o campo de CEP (formatação dinâmica)
-UnaryOperator<TextFormatter.Change> cepFilter = change -> {
-    String text = change.getControlNewText().replaceAll("[^\\d]", ""); // Remove qualquer não numérico
-    StringBuilder formatted = new StringBuilder();
+        UnaryOperator<TextFormatter.Change> cepFilter = change -> {
+            String text = change.getControlNewText().replaceAll("[^\\d]", ""); // Remove qualquer não numérico
+            StringBuilder formatted = new StringBuilder();
 
-    // Formata como "00000-000"
-    if (text.length() > 5) {
-        formatted.append(text.substring(0, 5)).append("-");
-        formatted.append(text.substring(5, Math.min(text.length(), 8)));
-    } else {
-        formatted.append(text);
-    }
-
-    // Atualiza o texto
-    change.setText(formatted.toString());
-    change.setRange(0, change.getControlText().length()); // Substitui tudo
-    return change;
-};
-
-TextFormatter<String> textFormatterCep = new TextFormatter<>(cepFilter);
-cepField.setTextFormatter(textFormatterCep);
-
-    // Máscara para o campo de CPF (formatação dinâmica)
-    UnaryOperator<TextFormatter.Change> cpfFilter = change -> {
-        String text = change.getControlNewText().replaceAll("[^\\d]", ""); // Remove qualquer não numérico
-        StringBuilder formatted = new StringBuilder();
-
-        // Formata como "000.000.000-00"
-        if (text.length() > 3) {
-            formatted.append(text.substring(0, 3)).append(".");
-            if (text.length() > 6) {
-                formatted.append(text.substring(3, 6)).append(".");
-                if (text.length() > 9) {
-                    formatted.append(text.substring(6, 9)).append("-");
-                    formatted.append(text.substring(9, Math.min(text.length(), 11)));
-                } else {
-                    formatted.append(text.substring(6));
-                }
+            // Formata como "00000-000"
+            if (text.length() > 5) {
+                formatted.append(text.substring(0, 5)).append("-");
+                formatted.append(text.substring(5, Math.min(text.length(), 8)));
             } else {
-                formatted.append(text.substring(3));
+                formatted.append(text);
             }
-        } else {
-            formatted.append(text);
-        }
 
             // Atualiza o texto
             change.setText(formatted.toString());
             change.setRange(0, change.getControlText().length()); // Substitui tudo
             return change;
-    };
+        };
 
-    TextFormatter<String> textFormatterCpf = new TextFormatter<>(cpfFilter);
-    cpfField.setTextFormatter(textFormatterCpf);
+        TextFormatter<String> textFormatterCep = new TextFormatter<>(cepFilter);
+        cepField.setTextFormatter(textFormatterCep);
 
-}
+            // Máscara para o campo de CPF (formatação dinâmica)
+        UnaryOperator<TextFormatter.Change> cpfFilter = change -> {
+                String text = change.getControlNewText().replaceAll("[^\\d]", ""); // Remove qualquer não numérico
+                StringBuilder formatted = new StringBuilder();
+
+                // Formata como "000.000.000-00"
+                if (text.length() > 3) {
+                    formatted.append(text.substring(0, 3)).append(".");
+                    if (text.length() > 6) {
+                        formatted.append(text.substring(3, 6)).append(".");
+                        if (text.length() > 9) {
+                            formatted.append(text.substring(6, 9)).append("-");
+                            formatted.append(text.substring(9, Math.min(text.length(), 11)));
+                        } else {
+                            formatted.append(text.substring(6));
+                        }
+                    } else {
+                        formatted.append(text.substring(3));
+                    }
+                } else {
+                    formatted.append(text);
+                }
+
+                    // Atualiza o texto
+                    change.setText(formatted.toString());
+                    change.setRange(0, change.getControlText().length()); // Substitui tudo
+                    return change;
+            };
+
+        TextFormatter<String> textFormatterCpf = new TextFormatter<>(cpfFilter);
+        cpfField.setTextFormatter(textFormatterCpf);
+
+        cpfField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (!newValue){
+                    if(cpfField.getText().isEmpty()) {
+                        cpfField.getStyleClass().add("errorInput");
+                    }
+                }else{
+                        if(!cpfField.getText().isEmpty()){
+                            cpfField.getStyleClass().remove("errorInput");
+                        }
+                }
+            }
+        });
+
+        cepField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (!newValue){
+                    if(cepField.getText().isEmpty()){
+                        cepField.getStyleClass().add("errorInput");
+                    }else if(cepField.getText().length()==9){
+                        String sql="SELECT DESCRICAO,COMPLEMENTO,DESCRICAO_CIDADE,UF FROM logradouro WHERE CEP = "+cepField.getText().replaceAll("-","")+";";
+                        try {
+                            Database.connect();
+                            PreparedStatement ps = Database.getConnection().prepareStatement(sql);
+                            ResultSet rs = ps.executeQuery();
+                            String e ="";
+                            ObservableList c = FXCollections.observableArrayList();
+                            String u = "";
+
+                            e=rs.getString(1)+" "+rs.getString(2);
+                            c.add(rs.getString(3));
+                            u=rs.getString(4);
+
+                            Database.close();
+                            addressField.setText(e);
+                            stateCombo.setValue(u);
+                            cityCombo.setItems(c);
+                        }catch (SQLException e){
+                            System.out.println(e);
+                        }
+                    }
+                }else{
+                    if(!cepField.getText().isEmpty()){
+                        cepField.getStyleClass().remove("errorInput");
+                    }
+                }
+            }
+        });
+
+        nameField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (!newValue){
+                    if(nameField.getText().isEmpty()){
+                        nameField.getStyleClass().add("errorInput");
+                    }
+                }else{
+                    if(!nameField.getText().isEmpty()){
+                        nameField.getStyleClass().remove("errorInput");
+                    }
+                }
+            }
+        });
+
+        emailField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (!newValue){
+                    if(emailField.getText().isEmpty()){
+                        //Credit to www.regular-expressions.info
+                        if(!emailField.getText().matches("^(?=[A-Za-z0-9][A-Za-z0-9@._%+-]{5,253}+$)[A-Za-z0-9._%+-]{1,64}+@" +
+                                "(?:(?=[A-Za-z0-9-]{1,63}+\\.)[A-Za-z0-9]++(?:-[A-Za-z0-9]++)*+\\.){1,8}+[A-Za-z]{2,63}+$")) {
+                            emailField.getStyleClass().add("errorInput");
+                        }
+                    }
+                }else{
+                    if(!emailField.getText().isEmpty()){
+                        emailField.getStyleClass().remove("errorInput");
+                    }
+                }
+            }
+        });
+
+        passwordField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (!newValue){
+                    if(passwordField.getText().isEmpty()){
+                        passwordField.getStyleClass().add("errorInput");
+                    }
+                }else{
+                    if(!passwordField.getText().isEmpty()){
+                        passwordField.getStyleClass().remove("errorInput");
+                    }
+                }
+            }
+        });
+
+        addressField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (!newValue){
+                    if(addressField.getText().isEmpty()){
+                        addressField.getStyleClass().add("errorInput");
+                    }
+                }else{
+                    if(!addressField.getText().isEmpty()){
+                        addressField.getStyleClass().remove("errorInput");
+                    }
+                }
+            }
+        });
+
+        ObservableList<String> uf =  FXCollections.observableArrayList("AC","AL","AM","AP","BA");
+
+        stateCombo.setItems(uf);
+        stateCombo.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (!newValue){
+                    if(stateCombo.getSelectionModel().isEmpty()){
+                        stateCombo.getStyleClass().add("errorInput");
+                    }
+                }else{
+                    if(!stateCombo.getSelectionModel().isEmpty()){
+                        stateCombo.getStyleClass().remove("errorInput");
+                    }
+                }
+            }
+        });
+
+    }
 
     // Métodos para navegação entre telas
     @FXML
